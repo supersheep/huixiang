8c590021990d5a233b300f6927245246429c657a
supersheep <supersheepxjy@gmail.com>
2013-05-04 16:48:51 +0800

    add external link for piece page

--- controller/page.py
+++ controller/page.py
@@ -49,19 +49,21 @@ class piece(base):
     def GET(self,id):
         """ piece """
         super(piece,self).GET()
-        pieces = db.select("piece",what="id,content",where="id=$id",vars={"id":id})
+        pieces = db.select("piece",what="id,content,link",where="id=$id",vars={"id":id})
+        cur_user = self.cur_user
+        cur_user_id = cur_user and cur_user["id"] or 0
         if not pieces:
             return web.notfound("oops")
 
         curpiece = pieces[0]
 
-        favs = db.select(["fav","user"],what="avatar,user.id", where="fav.userid=user.id and fav.pieceid=$id",vars={"id":id}, limit=5)
+        favs = db.select(["fav","user"],what="avatar,user.id", where="fav.userid=user.id and fav.pieceid=$id and fav.userid<>$cur_user_id",vars={"id":id,"cur_user_id":cur_user_id}, limit=5)
         
         liked = False
         where = {"id":id}
-        if self.cur_user:
+        if cur_user:
             where["userid"] = self.cur_user.id;
-        if self.cur_user and db.select("fav",what="id",where="fav.userid=$userid and pieceid=$id",vars=where):
+        if cur_user and db.select("fav",what="id",where="fav.userid=$userid and pieceid=$id",vars=where):
             liked = True
         return render.piece(curpiece,favs,liked)
 
