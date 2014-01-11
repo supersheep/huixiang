dbae506c812cbc3aebe721d201d6872743067cfa
supersheep <supersheepxjy@gmail.com>
2014-01-11 23:54:45 +0800

    add grunt heroku task

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,4 +39,5 @@ module.exports = function(grunt){
     grunt.loadNpmTasks("grunt-contrib-watch");
 
     grunt.registerTask("default",["jshint","stylus"]);
+    grunt.registerTask('heroku:production', 'jshint stylus');
 };
